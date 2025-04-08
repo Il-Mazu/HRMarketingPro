0303b7d9c3cb83bfd2f0dde20b25dc759a5054da
IlMazu <mazurekmarco06@gmail.com>
2025-04-08 23:41:42 +0200

    Update vite config with correct base URL

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/HRMarketingPro/', // Replace with your repo name
+  base: '/HRMarketingPro/', // Matches your repository name
   build: {
     outDir: '../dist/client',
     emptyOutDir: true
